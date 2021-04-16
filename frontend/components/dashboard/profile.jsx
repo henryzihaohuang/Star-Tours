bcfa534a0482cbebdc50dc2334c6e63372b1faef
henryzihaohuang <atlas.bb.contact@gmail.com>
2021-04-16 16:21:54 -0500

    Include google domain verification file for OAUTH

--- frontend/components/dashboard/profile.jsx
+++ frontend/components/dashboard/profile.jsx
@@ -6,21 +6,21 @@ import Col from 'react-bootstrap/Col';
 function Profile() {
     const currentUser = useSelector(state => state.currentUser);
 
-    debugger;
-    return(
-        <Row className="profile-sidebar-container">
-            <Col className="profile-sidebar-subcontainer">
-                <div className="profile-sidebar">
-                    <div className="profile-sidebar-elements">
-                        <img src={currentUser.imageUrl} alt="current-user-profile-picture"/>
-                        Name: {currentUser.firstName} + {currentUser.lastName}
-                        E-mail: {currentUser.email}
-                    </div>
+    // debugger;
+    // return(
+    //     <Row className="profile-sidebar-container">
+    //         <Col className="profile-sidebar-subcontainer">
+    //             <div className="profile-sidebar">
+    //                 <div className="profile-sidebar-elements">
+    //                     <img src={currentUser.imageUrl} alt="current-user-profile-picture"/>
+    //                     Name: {currentUser.firstName} + {currentUser.lastName}
+    //                     E-mail: {currentUser.email}
+    //                 </div>
 
-                </div>
-            </Col>
-        </Row>
-    )
+    //             </div>
+    //         </Col>
+    //     </Row>
+    // )
 };
 
 export default Profile;
