29320752b91b8aa8132c897919ab6e9e44980b07
henryzihaohuang <atlas.bb.contact@gmail.com>
2021-04-19 00:16:16 -0500

    Fix redirect error

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Splash from './splash/Splash';
 import Dashboard from './dashboard/Dashboard';
+import { Switch } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../utils/routeUtil';
 
 const App = ({state}) => {
     return (
         <div>
-            <AuthRoute exact path="/" component={Splash} />
-            <ProtectedRoute exact path="/dashboard" component={Dashboard} />
+            <Switch>
+                <AuthRoute exact path="/" component={Splash} />
+                <ProtectedRoute exact path="/dashboard" component={Dashboard} />
+            </Switch>
         </div>
     )
 };
